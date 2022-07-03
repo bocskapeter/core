3ad727ca67a23cc824a61da0dfcac351a30880b4
bocskapeter <bocska.peter@gmail.com>
2022-07-03 08:37:02 +0200

    Initial commit.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,11 @@ module eu.bopet.jocadv.core.core {
     requires commons.math3;
     requires org.apache.commons.lang3;
     exports eu.bopet.jocadv.core.features;
+    exports eu.bopet.jocadv.core.features.basic;
+    exports eu.bopet.jocadv.core.features.datums;
+    exports eu.bopet.jocadv.core.features.exception;
+    exports eu.bopet.jocadv.core.features.protrusion;
+    exports eu.bopet.jocadv.core.features.sketch;
     exports eu.bopet.jocadv.ie.step;
     exports eu.bopet.jocadv.ie.step.exception;
 }
